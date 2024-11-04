e5581911cad528ba98cbbae415dea80e94a1e19e
phoebeverdon <phoebeverdon@my.capilanou.ca>
2024-11-04 12:00:46 -0800

    Added about me_ gif, tweaked radial gradient, tweaked header and got rid of old menu button, tweaked new menu

--- js/script.js
+++ js/script.js
@@ -5,5 +5,5 @@ $(document).mousemove(function(event) {
   mouseXpercentage = Math.round(event.pageX / windowWidth * 100);
   mouseYpercentage = Math.round(event.pageY / windowHeight * 100);
   
-  $('.radial-gradient').css('background', 'radial-gradient(at ' + mouseXpercentage + '% ' + mouseYpercentage + '%, #ffc9dc, #fff9f3)');
+  $('.radial-gradient').css('background', 'radial-gradient(at ' + mouseXpercentage + '% ' + mouseYpercentage + '%, #ffd8e6, #fff9f3)');
 });
